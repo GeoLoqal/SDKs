a9229eae495300fb01f3102a60acecb398206e51
Ron@GeoLoqal <support@geoloqal.com>
2012-12-10 19:00:33 -0500

    Fixes for URLs

--- iOS/GLLocationManager.h
+++ iOS/GLLocationManager.h
@@ -3,45 +3,64 @@
 //  GeoLoqal
 //
 //  Created by user on 30/10/12.
-//  Copyright (c) 2012 GeoLoqal LLC. All rights reserved.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import "GeoLoqal.h"
 #import <CoreLocation/CoreLocation.h>
 #import "GLLocationManager.h"
 
+
+typedef struct {
+	double latitude;
+	double longitude;
+} GLLocationCoordinate2D;
+
 @protocol GeoLoqalDelegate <NSObject>
+
+@required
+-(void)geoLoqalPointLatitude :(double)lat longitude:(double)lon;
+
 @optional
--(void)geoPointLatitude :(double)lat longitude:(double)lon;
 -(void)testCaseName :(NSMutableArray *)testCasenames;
--(void)triggerName :(NSMutableArray *)triggerNames;
--(void)triggerStatus :(NSString *)triggerStatus;
--(void)byAddressLatitude :(double)lat longitude:(double)lon;
--(void)bySequentialPolylineLatitude :(double)lat longitude:(double)lon;
 -(void)createTriggerStatus :(NSString *)triggerStatus;
+-(void)triggerName :(NSMutableArray *)triggerNames; 
+-(void)triggerStatus :(NSString *)triggerStatus;
 
 @end
 
 @interface GLLocationManager : NSObject<CLLocationManagerDelegate>
+
 @property (nonatomic, strong) id<GeoLoqalDelegate> delegate;
+
+// give speed for GeoLoqal SDK
 @property(nonatomic)double speed;
-@property(nonatomic)int frequency;
 
--(id)initWithLatLon :(double)latitude :(double)longitude;
--(GLLocationCoordinate2D)registrationWithApiKey:(NSString *)apiKey andTestCase:(NSString *)testCaseName;
+// This is a mandatory property and the value is either json or xml
+@property(nonatomic,strong)NSString *outputType;
+
+// Sequential Polyline unit
+@property(nonatomic,strong)NSString *unit;
+
+// setting apikey from http://dev.geoloqal.com/ for GeoLoqal SDK
 +(void)setApiKey:(NSString *)geoloqalApiKey;
--(NSMutableArray *)getTestCaseNames;
--(GLLocationCoordinate2D)getPoints;
--(GLLocationCoordinate2D)getNextPoints:(NSString *)testCaseName;
--(GLLocationCoordinate2D)getDirectionPoints:(NSString *)testCaseName;
--(NSString *)getTestTypes;
--(NSString *)getTestTypes :(NSString *)_testType;
 
--(NSMutableArray *)getTriggerNames;
--(NSString *)getCheckedGeoTrigger:(NSString *)triggerName lat:(NSString *)latitude lon:(NSString *)longitude;
--(NSString *)createInsideCircleTrigger:(NSString *)triggerName lat:(NSString *)latitude lon:(NSString *)longitude rad:(int)_radius;
+// Register to GeoLoqal SDK for ios with an api key 
+-(void)registrationWithTestCase:(NSString *)testCaseName;
 
--(void)timeIntervalCallOfDirection;
--(void)timeIntervalCallOfNextpoint;
+// get all TestCaseNames for GeoLoqal SDK
+-(void)getTestCaseNames;
+
+// For Triggers //
+
+// get all Trigger names
+-(void)getTriggerNames;
+
+//method for checking  whether latitude and loangitude points reside inside a particular Trigger
+-(void)getCheckedGeoTriggerName:(NSString *)triggerName lat:(NSString *)latitude lon:(NSString *)longitude;
+
+// create a Trigger for "Inside_Circle" trigger type
+-(void)createInsideCircleTrigger:(NSString *)triggerName lat:(NSString *)latitude lon:(NSString *)longitude rad:(int)_radius;
+
+
 @end
